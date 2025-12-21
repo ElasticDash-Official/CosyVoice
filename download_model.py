e55b8ef7840f17d4e7659fbe2b00173d3cdc8579
TerryAtRocketVentures <68800619+terryjiang2020@users.noreply.github.com>
2025-12-21 15:49:49 +1300

    update download_model.py to download CosyVoice-300M-SFT model

--- download_model.py
+++ download_model.py
@@ -1,6 +1,3 @@
 from huggingface_hub import snapshot_download
-snapshot_download('FunAudioLLM/CosyVoice-300M', local_dir='pretrained_models/CosyVoice-300M')
 snapshot_download('FunAudioLLM/CosyVoice-300M-SFT', local_dir='pretrained_models/CosyVoice-300M-SFT')
-snapshot_download('FunAudioLLM/CosyVoice-300M-Instruct', local_dir='pretrained_models/CosyVoice-300M-Instruct')
-snapshot_download('FunAudioLLM/CosyVoice-ttsfrd', local_dir='pretrained_models/CosyVoice-ttsfrd')
 print("Model downloaded successfully!")
